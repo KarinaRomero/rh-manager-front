d90fa6e27419a9e938841fcaebf020c94fa144fb
fenix <betzy_skamm@hotmail.com>
2018-12-16 19:18:36 -0800

    added templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormEmployeeComponent } from './components/employees/components/form-employee/form-employee.component';
+import { FormSkillComponent } from './components/skills/components/form-skill/form-skill.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'employees',
+    component: EmployeesComponent
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent,
+    children: [
+      {
+        path: 'skills-add',
+        component: FormSkillComponent
+      }
+
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/employees',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EmployeesComponent,
+    SkillsComponent,
+    FooterComponent,
+    FormEmployeeComponent,
+    FormSkillComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
